d9630a735644486b080e6b6468e73a557db2b1b5
Russell Matney <russell.matney@gmail.com>
2015-04-07 01:33:51 -0400

    mp4s-to-timelapse rewrite

--- index.js
+++ index.js
@@ -11,65 +11,55 @@ var AWS = require('aws-sdk');
 var s3 = new AWS.S3();
 
 exports.handler = function(event, context) {
-  var start = new Date();
-  var pngsDownloaded;
-  var pngsRenamed;
-  var mp4sCreated;
-  var mp4sStitched;
-  var mp4Uploaded;
-  var result = {};
+  var result = event;
 
-  console.log(require('os').cpus().length);
-  //create /tmp/pngs/
-  execute(result, {
-    shell: 'mkdir -p /tmp/pngs/; mkdir -p /tmp/renamed-pngs/;',
-    logOutput: true
+  validate(result, {
+    "bucket": true,
+    "songKey": true,
+    "prefix": true
   })
 
-  /*
   .then(function(result) {
     return execute(result, {
-      shell: 'echo "ls /tmp/"; ls /tmp/; echo "ls /var/task/"; ls /var/task/;',
-      logOutput: true
+      shell: 'mkdir -p /tmp/mp4s'
     })
   })
-  */
 
+  //download song
   .then(function(result) {
     return download(result, {
-      srcBucket: event.bucket,
-      srcKey: event.songKey,
+      srcBucket: result.bucket,
+      srcKey: result.songKey,
       downloadFilepath: '/tmp/song.mp3'
     });
   })
 
-  //download pngs
+  //download mp4s
   .then(function(result) {
-    console.log('getting png keys');
+    console.log('getting mp4 keys');
     var def = Q.defer();
 
     s3.listObjects({
-      Bucket: event.bucket,
-      Prefix: event.prefix
+      Bucket: result.bucket,
+      Prefix: result.prefix
     }, function(err, data) {
       if (err) def.reject(err);
       else {
 
         var keys = data.Contents.map(function(object) {
-          if (/\.png$/.test(object.Key))
+          if (/\.mp4$/.test(object.Key))
             return object.Key;
         })
         keys = keys.filter(function(v) { return v; });
 
-        keys = keys.slice(0, event.fileNum || 100)
-        console.log('downloading ' + keys.length + ' pngs');
+        console.log(keys.length + ' mp4s found');
 
         var promises = [];
         keys.forEach(function(key) {
           promises.push(download(result, {
-            srcBucket: event.bucket,
+            srcBucket: result.bucket,
             srcKey: key,
-            downloadFilepath: '/tmp/pngs/' + path.basename(key)
+            downloadFilepath: '/tmp/mp4s/' + path.basename(key)
           }))
         });
 
@@ -87,66 +77,17 @@ exports.handler = function(event, context) {
     return def.promise;
   })
 
-  //rename, mv pngs
+  //endcard
   .then(function(result) {
-    pngsDownloaded = new Date();
-    console.log('renaming');
-    return execute(result, {
-      bashScript: '/var/task/rename-pngs',
-      bashParams: [
-        '/tmp/pngs/*.png',// input files
-        '/tmp/renamed-pngs/'//output dir
-      ],
-      logOutput: true
-    })
-  })
-
-  //convert pngs to mp4
-  .then(function(result) {
-    var def = Q.defer();
-    pngsRenamed = new Date();
-    console.log('creating timelapse');
-
-    var promises = [];
-    promises.push(execute(result, {
-      bashScript: '/var/task/files-to-mp4',
-      bashParams: [
-        '/tmp/renamed-pngs/%04d.png',//input files
-        '/tmp/timelapse1.mp4'//output filename
-      ],
-      logOutput: true
-    }))
-
-    promises.push(execute(result, {
-      bashScript: '/var/task/files-to-mp4',
-      bashParams: [
-        '/tmp/renamed-pngs/%04d.png',//input files
-        '/tmp/timelapse2.mp4'//output filename
-      ],
-      logOutput: true
-    }))
-
-    Q.all(promises)
-      .then(function(results) {
-        def.resolve(results[0]);
-      })
-      .fail(function(err) {
-        def.reject(err);
-      });
-
-    return def.promise;
+    return result;
   })
 
   //stitch mp4s together
   .then(function(result) {
-    mp4sCreated = new Date();
-
     return execute(result, {
       bashScript: '/var/task/stitch-mp4s',
       bashParams: [
-        //TODO: handle input videos as array/dir
-        '/tmp/timelapse1.mp4', //input
-        '/tmp/timelapse2.mp4', //input
+        '/tmp/mp4s/**.mp4', //mp4s dir
         '/tmp/song.mp3', //input song
         '/tmp/timelapse-final.mp4' //output filename
       ],
@@ -156,42 +97,18 @@ exports.handler = function(event, context) {
 
   //upload timelapse
   .then(function(result) {
-    mp4sStitched = new Date();
     console.log('uploading');
     return upload(result, {
-      dstBucket: event.bucket,
-      dstKey: event.prefix + '----timelapse.mp4',
+      dstBucket: result.bucket,
+      dstKey: result.prefix + '--timelapse-final.mp4',
       uploadFilepath: '/tmp/timelapse-final.mp4'
-    })
-  })
-
-  //clean up
-  .then(function(result){
-    mp4Uploaded = new Date();
-    return execute(result, {
-      shell: 'rm /tmp/renamed-pngs/*; rm /tmp/timelapse*',
-      logOutput: true
     })
   })
 
   .then(function(result){
     console.log('result');
+    console.log(result);
     context.done()
-
-    console.log('total duration');
-    console.log((new Date()).getTime() - start.getTime());
-    console.log('start -> pngs downloaded');
-    console.log(pngsDownloaded.getTime() - start.getTime());
-    console.log('pngs downloaded -> pngs renamed');
-    console.log(pngsRenamed.getTime() - pngsDownloaded.getTime());
-    console.log('renamed -> mp4sCreated');
-    console.log(mp4sCreated.getTime() - pngsRenamed.getTime());
-    console.log('mp4s -> stitched');
-    console.log(mp4sStitched.getTime() - mp4sCreated.getTime());
-    console.log('stitched -> uploaded');
-    console.log(mp4Uploaded.getTime() - mp4sStitched.getTime());
-    console.log('uploaded -> finished');
-    console.log((new Date()).getTime() - mp4Uploaded.getTime());
 
   }).fail(function(err) {
     console.log('errorrrrrr');
